75abd35d1945fa2c2356c5301a53034c491493b1
Arvind Yadav <arvindueyadav@gmail.com>
2017-03-05 18:55:31 +0530

    Login

--- controller/index.js
+++ controller/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express.Router();
+var jwt = require('jwt-simple');
+var moment = require('moment');
 var fs = require('fs');
+var config = {};
+config.TOKEN_SECRET = process.env.TOKEN_SECRET || 'nk235jaih535lhgdszhdfb-89ddsaj';
 
   app.get("/getAllCategory",function (req,res) {
     fs.readFile("./json/allCategory.json", 'utf8', function(err, data) {
@@ -21,5 +25,20 @@ var fs = require('fs');
         }
     });
   });
+app.post("/login",function(req,res){
+	var crdtl = req.body;
+	console.log(crdtl)
+	if(crdtl.username==="admin"&& crdtl.password==="password"){
+	var payload = {
+        	sub: crdtl.username,
+        	iat: moment().unix(),
+        	exp: moment().add(14, 'minutes').unix()
+    	};
+    
+	res.send({"message":"User Logged in Successfully","token":jwt.encode(payload, config.TOKEN_SECRET)});
+	}else{
+	res.status(401).send({"message":"User not Exist","token":null});
+	}
+});
 
 module.exports = app;
